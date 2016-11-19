a1e5f8b0e73e7c9bed87073819877a10e1a929f9
fzerorubigd <fzerorubigd@gmail.com>
2016-11-19 17:27:41 +0330

    lint

--- src/clickworker/worker.go
+++ src/clickworker/worker.go
@@ -1,14 +1,34 @@
+// Package main
+// Fraud is for checking fraud based on this rules
+//    ***Historical rules***
+//1.   duplicate click: one user by same imps, get two click on one ads
+//2.  unknown reference: when imp not have reference address
+//9.  fast clicks: under 4 second click
+//3.  Extra Cookie Active OR 3 clicks in month
+//5.  One Month Cookie Block: after active "Extra Cookie Active" all click fault
+//16.  There is no ad; ad id is not valid
+//4.  total click 4 per day
+//17.  one person before clicked on ads of same campaigns on day
+//
+//    ***new rules impression***
+//    mega impression same in select & show
+//
+//
+//    ***new rules click***
+//    same impersion_id in impression & click
+//    same ip in impression & click
+//
 package main
 
 import (
 	"assert"
+	"config"
 	"fmt"
 	"mr"
 	"redis"
 	"strconv"
 	"transport"
 	"utils"
-	"config"
 )
 
 // error means Ack/Nack the boolean maens only when error is not nil, and means re-queue
@@ -66,31 +86,6 @@ func clickWorker(in *transport.Click) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	err=aredis.HMSet(fmt.Sprintf("%s%s%s", transport.CONV, transport.DELIMITER, in.Rand),config.Config.Clickyab.DailyClickExpire,"CLICK",in.ID)
-	if err!=nil{
-		return false, err
-	}
-
-	//
-	return false, nil
+	err = aredis.HMSet(fmt.Sprintf("%s%s%s", transport.CONV, transport.DELIMITER, in.Rand), config.Config.Clickyab.DailyClickExpire, "CLICK", in.ID)
+	return false, err
 }
-
-// Fraud is for checking fraud based on this rules
-//    ***Historical rules***
-//1.   duplicate click: one user by same imps, get two click on one ads
-//2.  unknown reference: when imp not have reference address
-//9.  fast clicks: under 4 second click
-//3.  Extra Cookie Active OR 3 clicks in month
-//5.  One Month Cookie Block: after active "Extra Cookie Active" all click fault
-//16.  There is no ad; ad id is not valid
-//4.  total click 4 per day
-//17.  one person before clicked on ads of same campaigns on day
-//
-//    ***new rules impression***
-//    mega impression same in select & show
-//
-//
-//    ***new rules click***
-//    same impersion_id in impression & click
-//    same ip in impression & click
-//
