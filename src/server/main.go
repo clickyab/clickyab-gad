f4af4e455af2797fb92faef12516dd6c50169c8e
fzerorubigd <fzerorubigd@gmail.com>
2016-11-22 12:23:51 +0330

    add profiler again

--- src/server/main.go
+++ src/server/main.go
@@ -9,17 +9,33 @@ import (
 
 	"redis"
 
+	"os"
+
+	"os/signal"
+	"syscall"
+
+	"utils"
+
 	"github.com/labstack/echo/engine/fasthttp"
+	"github.com/pkg/profile"
 )
 
 func main() {
 	config.Initialize()
 	config.SetConfigParameter()
+	defer profile.Start(profile.CPUProfile, profile.NoShutdownHook, profile.ProfilePath("./"+<-utils.ID))
 
 	version.PrintVersion().Info("Application started")
 	models.Initialize()
 	aredis.Initialize()
 	rabbit.Initialize()
 
-	_ = modules.Initialize(config.Config.MountPoint).Run(fasthttp.New(config.Config.Port))
+	go func() {
+		_ = modules.Initialize(config.Config.MountPoint).Run(fasthttp.New(config.Config.Port))
+	}()
+
+	sig := make(chan os.Signal, 5)
+
+	signal.Notify(sig, syscall.SIGABRT, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL, syscall.SIGQUIT)
+	<-sig
 }
