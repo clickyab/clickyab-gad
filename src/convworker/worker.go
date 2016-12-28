b485e1f54c7d5dce2467b36ac59a533961519915
fzerorubigd <fzerorubigd@gmail.com>
2016-12-28 12:42:15 +0330

    make conversion tracking code backward compatible

--- src/convworker/worker.go
+++ src/convworker/worker.go
@@ -6,12 +6,19 @@ import (
 	"fmt"
 	"mr"
 	"redis"
+	"strconv"
 	"time"
 	"transport"
 )
 
 // error means Ack/Nack the boolean maens only when error is not nil, and means re-queue
 func convWorker(in *transport.Conversion) (bool, error) {
+
+	_, err := strconv.ParseInt(in.ConvID, 10, 0)
+	if err == nil { // the conversion is the integer value, th old syste,
+		return false, mr.NewManager().InsertConversion(in.ConvID, in.ActionID)
+	}
+
 	m, err := aredis.HGetAllString(fmt.Sprintf("%s%s%s", transport.CONV, transport.DELIMITER, in.ConvID), true, config.Config.Clickyab.DailyClickExpire)
 	if err != nil {
 		return false, err
