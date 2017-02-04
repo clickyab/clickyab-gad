39adc74f3c6b50d3ac621498d3e1002b01817241
fzerorubigd <fzero@rubi.gd>
2017-02-05 01:12:56 +0330

    ifinalize the route

--- src/router/web/select.go
+++ src/router/web/select.go
@@ -1,11 +1,10 @@
 package web
 
 import (
+	"entity"
 	"fmt"
 	"net/http"
 	"regexp"
-
-	"entity"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -16,16 +15,15 @@ func extractSlice(imp entity.Impression) map[string]string {
 	res := make(map[string]string)
 	for key, value := range imp.Request().URL.Query() {
 		if slice := slotReg.FindStringSubmatch(key); len(slice) == 2 {
-			res[slice[1]] = value
+			res[slice[1]] = value[0]
 		}
 	}
 
-	//if imp.OS().Valid && imp.OS().Mobile {
-	//	slotPub := fmt.Sprintf("%d%s", imp.Source().ID(), webMobile)
-	//	slotPublic = append(slotPublic, slotPub)
-	//	sizeNumSlice[slotPub] = 8
-	//}
-	//return tc.slotSizeNormal(slotPublic, website.WID, sizeNumSlice)
+	if imp.OS().Valid && imp.OS().Mobile {
+		slotPub := fmt.Sprintf("%d1000", imp.Source().ID())
+		res[slotPub] = "300x250"
+	}
+	return res
 }
 
 func selectWeb(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
