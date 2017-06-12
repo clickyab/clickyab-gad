d5647115203d363a3fed5ad2ead4fd23917f43f6
fzerorubigd <fzerorubigd@gmail.com>
2017-06-12 14:44:15 +0430

    remove extra logs

--- src/config/vast.go
+++ src/config/vast.go
@@ -1,10 +1,6 @@
 package config
 
-import (
-	"strconv"
-
-	"github.com/Sirupsen/logrus"
-)
+import "strconv"
 
 const (
 	short = "short"
@@ -13,21 +9,21 @@ const (
 )
 
 var vastConfig = map[string]map[string][]string{
-	"short": map[string][]string{
-		"start": []string{"linear", "11", "00:00:10", "00:00:03"},
-		"end":   []string{"linear", "12", "00:00:10", "00:00:03"},
+	"short": {
+		"start": {"linear", "11", "00:00:10", "00:00:03"},
+		"end":   {"linear", "12", "00:00:10", "00:00:03"},
 	},
-	"default": map[string][]string{
-		"start":    []string{"linear", "11", "00:00:10", "00:00:03"},
-		"00:00:10": []string{"non-linear", "22", "00:00:12"},
-		"end":      []string{"linear", "13", "00:00:10", "00:00:03"},
+	"default": {
+		"start":    {"linear", "11", "00:00:10", "00:00:03"},
+		"00:00:10": {"non-linear", "22", "00:00:12"},
+		"end":      {"linear", "13", "00:00:10", "00:00:03"},
 	},
-	"long": map[string][]string{
-		"start":    []string{"linear", "11", "00:00:10", "00:00:03"},
-		"00:00:20": []string{"non-linear", "22", "00:00:12"},
-		"00:01:20": []string{"non-linear", "23", "00:00:12"},
-		"00:03:20": []string{"non-linear", "24", "00:00:12"},
-		"00:05:20": []string{"non-linear", "25", "00:00:12"},
+	"long": {
+		"start":    {"linear", "11", "00:00:10", "00:00:03"},
+		"00:00:20": {"non-linear", "22", "00:00:12"},
+		"00:01:20": {"non-linear", "23", "00:00:12"},
+		"00:03:20": {"non-linear", "24", "00:00:12"},
+		"00:05:20": {"non-linear", "25", "00:00:12"},
 	},
 }
 
@@ -45,7 +41,6 @@ func MakeVastLen(len string, first string, mid string, end string) (string, map[
 		if mid != "" {
 			for j := range preRes[i] {
 				if j != "start" && j != "end" {
-					logrus.Info(j)
 					delete(preRes[i], j)
 				}
 			}
@@ -56,8 +51,6 @@ func MakeVastLen(len string, first string, mid string, end string) (string, map[
 	}
 	if m, err := strconv.ParseInt(len, 10, 64); err == nil {
 		if m < 30 {
-			logrus.Warn("<30")
-			logrus.Warn(preRes[short])
 			return short, preRes[short]
 		} else if m < 90 {
 			return def, preRes[def]
