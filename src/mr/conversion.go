c628c70bbe49122cf24ea5e184c101b965e90276
slifer <dara51php@gmail.com>
2017-09-18 15:59:02 +0430

    add conversion
    Fix #3299

--- src/mr/conversion.go
+++ src/mr/conversion.go
@@ -6,48 +6,36 @@ import (
 )
 
 // InsertConversion try to insert conversion base on click
-func (m *Manager) InsertConversion(clickID, action string) error {
-	q := `INSERT INTO clicks_conv (c_id,
-		w_id,
-		app_id,
-		wp_id,
-		ca_id,
-		ad_id,
-		cop_id,
-		cp_id,
-		slot_id,
-		sla_id,
-		imp_id,
-		c_ip,
-		c_status,
-		c_referaddress,
-		c_parenturl,
-		c_fast,
-		c_os,
-		c_winnerbid,
-		c_time,
-		c_date,
-		c_action)
-		SELECT c_id,
-		w_id,
-		app_id,
-		wp_id,
-		ca_id,
-		ad_id,
-		cop_id,
-		cp_id,
-		slot_id,
-		sla_id,
-		imp_id,
-		c_ip,
-		c_status,
-		c_referaddress,
-		c_parenturl,
-		c_fast,
-		c_os,
-		c_winnerbid, ?, ?, ? FROM clicks WHERE c_id=?`
+func (m *Manager) InsertConversion(actionID string, impression *Impression) error {
+	q := `INSERT INTO clicks_conv
+			(
+			w_id,
+			app_id,
+			wp_id,
+			ca_id,
+			ad_id,
+			cop_id,
+			cp_id,
+			slot_id,
+			imp_id,
+			c_time,
+			c_date,
+			c_action)
+			VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`
 
-	a, err := m.GetWDbMap().Exec(q, time.Now().Unix(), time.Now().Format("20060102"), action, clickID)
+	a, err := m.GetWDbMap().Exec(q,
+		impression.WebsiteID,
+		impression.AppID,
+		impression.WP,
+		impression.CaID,
+		impression.AdID,
+		impression.CopID,
+		impression.CPID,
+		impression.SlotID,
+		impression.ID,
+		time.Now().Unix(),
+		time.Now().Format("20060102"),
+		actionID)
 	if err != nil {
 		return err
 	}
